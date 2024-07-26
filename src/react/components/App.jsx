a6783157723061603cef29ed3178cf23da4eee31
Godjiee <joaocorreiapf23@gmail.com>
2024-07-26 21:25:01 +0100

    add window size and icons

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -1,27 +1,11 @@
 import {useState} from 'react';
 
 const App = () => {
-  const [scriptResp, setScriptResp] = useState(null);
-
-  const getRandomUser = async () => {
-    // Get users
-    const resp = await fetch('https://jsonplaceholder.typicode.com/users');
-    const users = await resp.json();
-    // Get email
-    const randomEmail = users[Math.floor(Math.random() * users.length)].email;
-    // Get active tab
-    const tabs = await chrome.tabs.query({active: true, currentWindow: true});
-    const activeTab = tabs[0];
-    // Get the response
-    const tabResp = await chrome.tabs.sendMessage(activeTab.id, randomEmail);
-    setScriptResp(tabResp);
-  };
 
   return (
     <main>
-      <h1>Add User Contact to Page</h1>
-      <button onClick={getRandomUser}>Get Random User</button>
-      <h2>Content script says: {scriptResp}</h2>
+      <h1>Solglyph</h1>
+      <button onClick={()=>{}}>BUY</button>
     </main>
   );
 };
