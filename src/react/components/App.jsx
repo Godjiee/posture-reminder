79b1d850672018fc4ac1e563a1f36a7212b8cdb5
Godjiee <joaocorreiapf23@gmail.com>
2024-08-07 13:17:28 +0100

    commit change pc

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -1,18 +1,29 @@
 import React, {useState} from 'react';
 import {
-  AppBar,
-  Tabs,
-  Tab,
   Typography,
   Box,
   Container,
   Pagination,
-} from '@mui/material';
+  InputBase,
+  InputAdornment,
+  IconButton,
+  CircularProgress,
+  Paper,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+} from '@mui/material'; // Import necessary components from MUI
+
+import SearchIcon from '@mui/icons-material/Search'; // Import SearchIcon from MUI icons
+
 import solglyph from '../assets/SolGlyph_android.png';
 import clout from '../assets/clout_logo.png';
 import nx from '../assets/nx_logo.png';
 import socrates from '../assets/socrates_logo.png';
 import grass from '../assets/grass_logo.png';
+
 import Details from './Details'; // Import the Details component
 import airdropData from '../assets/airdropData.json'; // Import the JSON file
 
@@ -20,14 +31,11 @@ const App = () => {
   const [value, setValue] = useState(0);
   const [selectedItem, setSelectedItem] = useState(null);
 
+  // Add state for search query
+  const [searchQuery, setSearchQuery] = useState('');
+
   const [page, setPage] = useState(1);
   const itemsPerPage = 10; // Set the number of items per page
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-    setSelectedItem(null); // Reset selected item when tab changes
-    setPage(1); // Reset to the first page when tab changes
-  };
 
   // Mapping title to corresponding logo
   const getLogo = title => {
@@ -60,40 +68,72 @@ const App = () => {
     setSelectedItem(item);
   };
 
+  // Function to handle search query changes
+  const handleSearchChange = event => {
+    setSearchQuery(event.target.value);
+  };
+
   if (selectedItem) {
     return <Details item={selectedItem} onBack={() => setSelectedItem(null)} />;
   }
 
   return (
-    <Box sx={{flexGrow: 1}}>
-      <AppBar
-        sx={{
-          background: '#161E2F',
-        }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label="simple tabs example"
-          centered
-          variant="fullWidth"
-          TabIndicatorProps={{style: {backgroundColor: '#B8B2A9'}}}>
-          <Tab
-            label="New"
-            sx={{color: '#B8B2A9', '&.Mui-selected': {color: '#D4D0CA'}}}
+    <Box sx={{flexGrow: 1, mt: 3, width: '100%'}}>
+      <Container maxWidth="sm">
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            mb: 1,
+            mx: -1,
+          }}>
+          <Box
+            component="img"
+            src={solglyph}
+            alt="img"
+            sx={{
+              width: '40px',
+              height: 'auto',
+              borderRadius: '8px',
+              boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
+            }}
           />
-          <Tab
-            label="Promoted"
-            sx={{color: '#B8B2A9', '&.Mui-selected': {color: '#D4D0CA'}}}
+
+          {/* Search Bar */}
+          <InputBase
+            sx={{
+              flexGrow: 1,
+              ml: 2, //
+              color: '#4E5861',
+              backgroundColor: '#0D1B2A',
+              borderRadius: '4px',
+              padding: '4px 8px',
+              fontSize: '14px',
+              boxShadow: '0 2px 4px rgba(0, 0, 0, 0.15)',
+            }}
+            placeholder="Search for airdrop"
+            inputProps={{'aria-label': 'search'}}
+            value={searchQuery}
+            onChange={handleSearchChange}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  type="submit"
+                  sx={{p: '8px', color: '#46ACC2'}}
+                  aria-label="search">
+                  <SearchIcon />
+                </IconButton>
+              </InputAdornment>
+            }
           />
-        </Tabs>
-      </AppBar>
-      <TabPanel value={value} index={0}>
+        </Box>
+
         <Typography
           sx={{
             fontSize: '18px',
             color: 'white',
             textAlign: 'center',
-            mt: 7,
             py: 2,
           }}>
           {newItems.length} AIRDROPS LISTED
@@ -105,16 +145,7 @@ const App = () => {
           setPage={setPage}
           itemsPerPage={itemsPerPage}
         />
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <ItemList
-          items={promotedItems}
-          onItemClick={handleItemClick}
-          page={page}
-          setPage={setPage}
-          itemsPerPage={itemsPerPage}
-        />
-      </TabPanel>
+      </Container>
     </Box>
   );
 };
@@ -129,145 +160,110 @@ const ItemList = ({items, onItemClick, page, setPage, itemsPerPage}) => {
   };
 
   return (
-    <Container maxWidth="sm">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          gap: 1,
-          borderRadius: 2,
-          pt: 1,
-          px: 1,
-          boxShadow: 3,
-        }}>
-        {/* Check if the items array is empty */}
-        {items.length === 0 ? (
-          <Typography
-            sx={{
-              color: '#fff',
-              fontSize: '15px',
-              textAlign: 'center',
-              mt: 5, // Add some margin for spacing
-              py: 8,
-            }}>
-            No promoted airdrops yet, visit solglyph to promote your airdrop.
-          </Typography>
-        ) : (
-          paginatedItems.map((item, index) => (
-            <Box
-              key={index}
-              onClick={() => onItemClick(item)}
-              sx={{
-                width: '100%',
-                border: '1px solid #2C7484',
-                borderRadius: 2,
-                p: 2,
-                display: 'flex',
-                alignItems: 'center',
-                backgroundColor: '#1F2937',
-                cursor: 'pointer',
-              }}>
-              <Box>
-                <img
-                  src={item.image}
-                  alt="img"
-                  style={{
-                    width: 40,
-                    height: 40,
-                    marginRight: 16,
-                    borderRadius: '50%', // Corrected borderRadius
-                    objectFit: 'cover', // Ensures the image covers the circle
-                  }}
-                />
-              </Box>
-              <Box>
-                <Typography sx={{color: '#fff'}}>{item.title}</Typography>
-                <Typography
-                  sx={{
-                    fontSize: '12px',
-                    color: '#D4D0CA',
-                    overflow: 'hidden',
-                    whiteSpace: 'nowrap',
-                    textOverflow: 'ellipsis',
-                    width: '230px',
-                  }}>
-                  {item.description}
-                </Typography>
-              </Box>
-            </Box>
-          ))
-        )}
-        {items.length > itemsPerPage && (
-          <Pagination
-            count={Math.ceil(items.length / itemsPerPage)}
-            page={page}
-            onChange={handlePageChange}
-            color="standard"
-            sx={{
-              mt: 2,
-              mb: 1,
-              '& .MuiPaginationItem-root': {
-                color: '#fff',
-              },
-              '& .Mui-selected': {
-                color: '#46ACC2',
-              },
-            }}
-          />
-        )}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 1,
+        borderRadius: 2,
+        pt: 1,
+        px: 1,
+        boxShadow: 3,
+      }}>
+      {paginatedItems.map((item, index) => (
         <Box
-          onClick={() => window.open('https://solglyph.com', '_blank')}
+          key={index}
+          onClick={() => onItemClick(item)}
           sx={{
             width: '100%',
             border: '1px solid #2C7484',
             borderRadius: 2,
-            px: 2,
+            p: 2,
             display: 'flex',
-            justifyContent: 'center',
             alignItems: 'center',
+            backgroundColor: '#1F2937',
             cursor: 'pointer',
-            minHeight: '48px',
-            mt: 2,
-            mb: 2,
-            background: 'linear-gradient(45deg, #40D7AC, #58E3D0, #6969E0)',
-            transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-            '&:hover': {
-              transform: 'scale(1.05)',
-              boxShadow: '0 0 15px rgba(70, 172, 194, 0.5)',
-            },
           }}>
-          <Typography
-            sx={{
-              fontSize: '18px',
-              color: 'white',
-              textAlign: 'center',
-              fontWeight: 'bold',
-            }}>
-            VISIT SOLGLYPH
-          </Typography>
+          <Box>
+            <img
+              src={item.image}
+              alt="img"
+              style={{
+                width: 40,
+                height: 40,
+                marginRight: 16,
+                borderRadius: '50%', // Corrected borderRadius
+                objectFit: 'cover', // Ensures the image covers the circle
+              }}
+            />
+          </Box>
+          <Box>
+            <Typography sx={{color: '#fff'}}>{item.title}</Typography>
+            <Typography
+              sx={{
+                fontSize: '12px',
+                color: '#D4D0CA',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+                width: '230px',
+              }}>
+              {item.description}
+            </Typography>
+          </Box>
         </Box>
-      </Box>
-    </Container>
-  );
-};
-
-const TabPanel = props => {
-  const {children, value, index, ...other} = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}>
-      {value === index && (
-        <Box sx={{p: 0}}>
-          <Typography sx={{color: 'white'}}>{children}</Typography>
-        </Box>
+      ))}
+      {items.length > itemsPerPage && (
+        <Pagination
+          count={Math.ceil(items.length / itemsPerPage)}
+          page={page}
+          onChange={handlePageChange}
+          color="standard"
+          sx={{
+            mt: 2,
+            mb: 1,
+            '& .MuiPaginationItem-root': {
+              color: '#fff',
+            },
+            '& .Mui-selected': {
+              color: '#46ACC2',
+            },
+          }}
+        />
       )}
-    </div>
+      <Box
+        onClick={() => window.open('https://solglyph.com', '_blank')}
+        sx={{
+          width: '100%',
+          border: '1px solid #2C7484',
+          borderRadius: 2,
+          px: 2,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          cursor: 'pointer',
+          minHeight: '48px',
+          mt: 2,
+          mb: 2,
+          background: 'linear-gradient(45deg, #40D7AC, #58E3D0, #6969E0)',
+          transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+          '&:hover': {
+            transform: 'scale(1.05)',
+            boxShadow: '0 0 15px rgba(70, 172, 194, 0.5)',
+          },
+        }}>
+        <Typography
+          sx={{
+            fontSize: '18px',
+            color: 'white',
+            textAlign: 'center',
+            fontWeight: 'bold',
+          }}>
+          VISIT SOLGLYPH
+        </Typography>
+      </Box>
+    </Box>
   );
 };
 
